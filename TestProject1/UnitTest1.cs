c267e8e98ab5f53392abfce675a60e30901275cb
LorandSzekely <LorandSzekely@LORAND-LAPTOP>
2024-03-22 18:04:03 +0200

    Add project files.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Manage().Window.Maximize();
+            IWebElement webElement = driver.FindElement(By.Id("L2AGLb"));
+            webElement.Click();
+            IWebElement webElement1 = driver.FindElement(By.Name("q"));
+            webElement1.SendKeys("apple");
+            webElement1.SendKeys(Keys.Return);
+        }
+    }
+}
